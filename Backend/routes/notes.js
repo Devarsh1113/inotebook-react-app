755dada5687a9eb83184c3c368ddae3005c57d9c
Devarsh Agrawal <dagrawal@evertz.com>
2024-02-26 16:14:43 -0500

    Added Schema and Demno Api Path.

---
+++ Backend/routes/notes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const { model } = require('mongoose');
+const router = express.Router();
+
+router.get('/', (req,res)=>{
+    obj ={
+        
+    }
+    res.json([])
+})
+module.exports = router
